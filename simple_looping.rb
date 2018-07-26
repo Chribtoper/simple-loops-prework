3b4440e57c97fd5b41408fcc563f10b30cae22eb
Chribtoper <xcstolfax@gmail.com>
2018-07-26 19:05:30 +0000

    automatically backed up by learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -11,7 +11,7 @@ def loop_iterator(number_of_times)
   phrase = "Welcome to Flatiron School's Web Development Course!"
   times = 0
   loop do 
-    phrase
+    puts phrase
     times += 1
      break if times == 7
   end
